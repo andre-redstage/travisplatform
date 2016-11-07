c64db4a45cd5b37003c26db97fe234a250d464e6
André Felipe <flp_andre@yahoo.com.br>
2016-11-07 17:43:49 -0200

    Test another way to get around prompt waitings

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,11 +10,14 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     chmod 600 .travis/development
     mv .travis/development ~/.ssh/id_rsa
 
-    touch ~/.ssh/config
+    #touch ~/.ssh/config
 
-    cat /home/travis/.ssh/config
+    #cat /home/travis/.ssh/config
     #echo -e "Host 142.54.227.126\n\tStrictHostKeyChecking no" >> /home/travis/.ssh/config
     #echo -e "Host 52.200.159.125\n\tStrictHostKeyChecking no" >> /home/travis/.ssh/config
+
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no uat.travisplatform@142.54.227.126
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no hjec3fcqxqntw@52.200.159.125
 
     # eval "$(ssh-agent -s)"
     # ssh-add ~/.ssh/deploy-key        
@@ -23,12 +26,12 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     git config user.name "Travis CI"
     git config user.email "travis@test.com"
 
-    git remote add deploy "ssh://travis@104.236.118.124//var/www/repotest/repotest.git"
-    git push -f deploy master
+    #git remote add deploy "ssh://travis@104.236.118.124//var/www/repotest/repotest.git"
+    #git push -f deploy master
 
     # Used for server, such as ZeroLag.
-    #git remote add deploy "ssh://uat.travisplatform@dev03.redstage.cl.zerolag.com//www/sites/uat.travisplatform/files/git"    
-    #git push -f deploy master
+    git remote add deploy "ssh://uat.travisplatform@dev03.redstage.cl.zerolag.com//www/sites/uat.travisplatform/files/git"    
+    git push -f deploy master
     # --
 
     # Used for Platform.sh
