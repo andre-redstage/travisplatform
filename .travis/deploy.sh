c9c684706e483ac5f92d840d2a91afec5c0a36e4
André Felipe <flp_andre@yahoo.com.br>
2016-11-07 14:28:59 -0200

    Travis added

---
+++ .travis/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# print outputs and exit on first failure
+set -xe
+
+if [ $TRAVIS_BRANCH == "master" ] ; then    
+    
+    openssl aes-256-cbc -K $encrypted_790cf80679f9_key -iv $encrypted_790cf80679f9_iv -in .travis/development.enc -out .travis/development -d
+    
+    chmod 600 .travis/development
+    mv .travis/development ~/.ssh/id_rsa
+
+    # eval "$(ssh-agent -s)"
+    # ssh-add ~/.ssh/deploy-key        
+    # chmod 600 ~/.ssh/deploy-key 
+
+    git config user.name "Travis CI"
+    git config user.email "travis@test.com"
+
+    # Used for server, such as ZeroLag.
+    git remote add deploy "ssh://uat.travisplatform@dev03.redstage.cl.zerolag.com//www/sites/uat.travisplatform/files/git"    
+    git push -f deploy master
+    # --
+
+    # Used for Platform.sh
+    git remote add platform "hjec3fcqxqntw@git.us.magento.cloud:hjec3fcqxqntw.git"
+    git push -f platform master
+    # --
+else
+
+    echo "No deploy script for branch '$TRAVIS_BRANCH'"
+
+fi
