bf0b4544a5d1ebe2c2dedd79ebe658c979a91de3
Andre Santos <asantos@redstage.com>
2016-11-16 16:13:17 -0200

    Update deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,6 +5,8 @@ openssl aes-256-cbc -K $encrypted_790cf80679f9_key -iv $encrypted_790cf80679f9_i
 chmod 600 .travis/development
 mv .travis/development ~/.ssh/id_rsa
 
+echo $TRAVIS_REPO_SLUG
+
 git config user.name "Travis CI"
 git config user.email "travis@test.com"
 
