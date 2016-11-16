e744b451651c88e5a181cef49abfaa197992a4b9
Andre Santos <asantos@redstage.com>
2016-11-16 16:36:01 -0200

    Update travis script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_790cf80679f9_key -iv $encrypted_790cf80679f9_iv -in .travis/development.enc -out .travis/development -d
-
-set -xe
-
-chmod 600 .travis/development
-mv .travis/development ~/.ssh/id_rsa
-
-echo $TRAVIS_REPO_SLUG
-echo $TRAVIS_PULL_REQUEST
-
-git config user.name "Travis CI"
-git config user.email "travis@test.com"
-
-if [ $TRAVIS_BRANCH == "master" ]
+#Only update the production/development server after PR has been accepted
+if [ $TRAVIS_PULL_REQUEST == "false" ]
 then
 
-    #Using Zerolag as production
-    git remote add deploy $PRODUCTION_REMOTE
-    git push -f deploy $TRAVIS_BRANCH    
+	openssl aes-256-cbc -K $encrypted_790cf80679f9_key -iv $encrypted_790cf80679f9_iv -in .travis/development.enc -out .travis/development -d
 
+	set -xe
+
+	chmod 600 .travis/development
+	mv .travis/development ~/.ssh/id_rsa
+
+	git config user.name "Travis CI"
+	git config user.email "travis@test.com"
+
+	if [ $TRAVIS_BRANCH == "master" ]
+	then
+
+	    #Using Zerolag as production
+	    git remote add deploy $PRODUCTION_REMOTE
+	    git push -f deploy $TRAVIS_BRANCH    
+
+	fi
+
+	git remote add platform $PLATFORM_REMOTE
+	git push -f platform $TRAVIS_BRANCH
 fi
-
-git remote add platform $PLATFORM_REMOTE
-git push -f platform $TRAVIS_BRANCH
